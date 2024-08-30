469698f65e16fdb973d8eb8c58f75f20dcf02f7f
El Mehdi Iddouch <foot4_6@hotmail.fr>
2024-08-30 23:00:00 +0100

    "Projet Setup validated"

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve:{
-    alias:{
-      "@": path.resolve
-      (__dirname, "./src"),
+  define: {
+    "process.env": {
+      NODE_ENV: "production",
     },
   },
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: "./src/index.jsx",
+      name: "widget",
+      fileName: (format) => `widget.${format}.js`,
+    },
+    target: "esnext",
+  },
+});
